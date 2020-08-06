0466bd0d7c3185e79226e05cdb87ff533d0c9ab0
gyq-study <1049309978@qq.com>
2020-08-06 16:09:05 +0800

    update Person.java 2

--- src/main/java/com/hqyj/gyq/entity/Person.java
+++ src/main/java/com/hqyj/gyq/entity/Person.java
@@ -1,4 +1,13 @@
 package com.hqyj.gyq.entity;
 
 public class Person {
+    private int pid;
+
+    public int getPid() {
+        return pid;
+    }
+
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
 }
